2e932a29b356a672e90f398bf3ca4549fa346248
mattiparro <mattyparro@live.it>
2016-11-27 18:22:32 +0100

    Aggiornamento Database
    Ho rifatto il db con i campi nuovi e ho provato ad inserire i tipi che
    dice sul sito, per lanciarlo devi entrare nella bash di postgresql, fare
    CREATE DATABASE Blockchain; e lanciare il comando \i
    C:percorso/del/db/Blockchain.sql

--- Database/Blockchain.sql
+++ Database/Blockchain.sql
@@ -1,54 +1,56 @@
 \c blockchain
 
 CREATE TABLE Block(
-	block_id varchar (35) NOT NULL PRIMARY KEY,
-	hash varchar (35) NOT NULL,
-	version varchar (35) NOT NULL,
-	prev_block varchar (35) NOT NULL  REFERENCES Block (block_id),
-	mrkl_root varchar (35) NOT NULL,
-	time varchar (35) NOT NULL,
-	bits varchar (35) NOT NULL,
-	free varchar (35) NOT NULL,
-	nonce varchar (35) NOT NULL,
-	n_tx varchar (35) NOT NULL,
-	size varchar (35) NOT NULL,
-	main_chain varchar (35) NOT NULL,
-	height varchar (35) NOT NULL
+	hash varchar (35) NOT NULL PRIMARY KEY,
+	confirmations INTEGER NOT NULL,
+	strippedsize  INTEGER NOT NULL,
+	size INTEGER NOT NULL,
+	weight INTEGER NOT NULL,
+	height INTEGER NOT NULL,
+	version INTEGER  NOT NULL,
+	versionHex varchar (35) NOT NULL,
+	merkleroot varchar (35) NOT NULL,
+	time INTEGER NOT NULL,
+	mediantime INTEGER NOT NULL,
+	nonce INTEGER NOT NULL,
+	bits varchar NOT NULL,
+	difficulty INTEGER NOT NULL,
+	chainwork varchar (35) NOT NULL,
+	previousblockhash varchar (35) NOT NULL REFERENCES Block (hash),
+	nextblockhash varchar (35) NOT NULL REFERENCES Block (hash)
 );
 
 CREATE TABLE Transaction(
-	tx_index varchar (35) NOT NULL PRIMARY KEY,
+	hex varchar (35) NOT NULL,
+	txid varchar (35) NOT NULL  PRIMARY KEY,
 	hash varchar (35) NOT NULL,
-	lock_time varchar (35) NOT NULL,
-	version varchar (35) NOT NULL,
-	size varchar (35) NOT NULL,
-	time varchar (35) NOT NULL,
-	vin_size varchar (35) NOT NULL,
-	vout_size varchar (35) NOT NULL,
-	relayed_by varchar (35) NOT NULL,
-	block_id varchar (35) NOT NULL REFERENCES Block (block_id)
+	size INTEGER NOT NULL,
+	vsize INTEGER NOT NULL,
+	version INTEGER  NOT NULL,
+	locktime INTEGER  NOT NULL,
+	blockhash varchar (35) NOT NULL  REFERENCES Block(hash),
+	confirmations INTEGER NOT NULL,
+	time INTEGER  NOT NULL,
+	blocktime INTEGER  NOT NULL
 );
 
-CREATE TABLE Input(
-	id_input varchar (35) NOT NULL PRIMARY KEY,
-	sequence varchar (35) NOT NULL,
-	tx_index varchar (35) NOT NULL REFERENCES Transaction (tx_index),
-	spent varchar (35) NOT NULL,
-	type varchar (35) NOT NULL,
-	addr varchar (35) NOT NULL,
-	value varchar (35) NOT NULL,
-	n varchar (35) NOT NULL,
-	script varchar (35) NOT NULL,
-	out_script varchar (35) NOT NULL 
+CREATE TABLE Tx_input(
+	txid varchar (35) NOT NULL REFERENCES Transaction(txid),
+	vout INTEGER NOT NULL,
+	asm varchar (35) NOT NULL,
+	hex varchar (35) NOT NULL,
+	sequence INTEGER NOT NULL,
+	CONSTRAINT txin PRIMARY KEY (txid,vout)
 );
 
 CREATE TABLE Output(
-	id_output varchar (35) NOT NULL PRIMARY KEY,
-	tx_index varchar (35) NOT NULL REFERENCES Transaction (tx_index),
-	spent varchar (35) NOT NULL,
+	txid varchar (35) NOT NULL REFERENCES Transaction (txid),
+	value FLOAT NOT NULL,
+	n INTEGER  NOT NULL,
+	asm varchar (35) NOT NULL,
+	hex varchar (35) NOT NULL,
+	reqSigs INTEGER  NOT NULL,
 	type varchar (35) NOT NULL,
-	addr varchar (35) NOT NULL,
-	value varchar (35) NOT NULL,
-	n varchar (35) NOT NULL,
-	out_script varchar (35) NOT NULL 
+	addresses varchar (35) NOT NULL,
+	CONSTRAINT txout PRIMARY KEY (txid,n)
 );
