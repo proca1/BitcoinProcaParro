139d2a7877cd8c39ff54b9163c8fc3dc59c4c443
mattiparro <mattyparro@live.it>
2016-11-28 13:54:33 +0100

    Inserimento parser e modifica varchar db
    Ho inserito i parser di bitlodine e modificato lunghezza varchar nel db

--- Database/Blockchain.sql
+++ Database/Blockchain.sql
@@ -1,54 +1,54 @@
 \c blockchain
 
 CREATE TABLE Block(
-	hash varchar (35) NOT NULL PRIMARY KEY,
+	hash varchar NOT NULL PRIMARY KEY,
 	confirmations INTEGER NOT NULL,
 	strippedsize  INTEGER NOT NULL,
 	size INTEGER NOT NULL,
 	weight INTEGER NOT NULL,
 	height INTEGER NOT NULL,
 	version INTEGER  NOT NULL,
-	versionHex varchar (35) NOT NULL,
-	merkleroot varchar (35) NOT NULL,
+	versionHex varchar NOT NULL,
+	merkleroot varchar NOT NULL,
 	time INTEGER NOT NULL,
 	mediantime INTEGER NOT NULL,
 	nonce INTEGER NOT NULL,
 	bits varchar NOT NULL,
 	difficulty INTEGER NOT NULL,
-	chainwork varchar (35) NOT NULL,
-	previousblockhash varchar (35) NOT NULL REFERENCES Block (hash),
-	nextblockhash varchar (35) NOT NULL REFERENCES Block (hash)
+	chainwork varchar NOT NULL,
+	previousblockhash varchar NOT NULL REFERENCES Block (hash),
+	nextblockhash varchar NOT NULL REFERENCES Block (hash)
 );
 
 CREATE TABLE Transaction(
-	hex varchar (35) NOT NULL,
-	txid varchar (35) NOT NULL  PRIMARY KEY,
-	hash varchar (35) NOT NULL,
+	hex varchar NOT NULL,
+	txid varchar NOT NULL  PRIMARY KEY,
+	hash varchar NOT NULL,
 	size INTEGER NOT NULL,
 	vsize INTEGER NOT NULL,
 	version INTEGER  NOT NULL,
 	locktime INTEGER  NOT NULL,
-	blockhash varchar (35) NOT NULL  REFERENCES Block(hash),
+	blockhash varchar NOT NULL  REFERENCES Block(hash),
 	confirmations INTEGER NOT NULL,
 	time INTEGER  NOT NULL,
 	blocktime INTEGER  NOT NULL
 );
 
 CREATE TABLE Tx_input(
-	txid varchar (35) NOT NULL REFERENCES Transaction(txid),
+	txid varchar  NOT NULL REFERENCES Transaction(txid),
 	vout INTEGER NOT NULL,
-	asm varchar (35) NOT NULL,
-	hex varchar (35) NOT NULL,
+	asm varchar  NOT NULL,
+	hex varchar NOT NULL,
 	sequence INTEGER NOT NULL,
 	CONSTRAINT txin PRIMARY KEY (txid,vout)
 );
 
 CREATE TABLE Tx_output(
-	txid varchar (35) NOT NULL REFERENCES Transaction (txid),
+	txid varchar NOT NULL REFERENCES Transaction (txid),
 	value FLOAT NOT NULL,
 	n INTEGER  NOT NULL,
-	asm varchar (35) NOT NULL,
-	hex varchar (35) NOT NULL,
+	asm varchar NOT NULL,
+	hex varchar NOT NULL,
 	reqSigs INTEGER  NOT NULL,
 	type varchar (35) NOT NULL,
 	addresses varchar (35) NOT NULL,
